8f37d525a05a722f92eb2ede7b34ffe6ad73bb72
chica25 <patricia@patriciaco.com>
2020-07-20 11:00:09 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,10 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/food_form' do
-    erb :food_form
-  end
+    get '/food_form' do
+      erb :food_form
+    end
+  
 
   # Add your post route and action below
-
+    post '/food' do
+      params.to_s
+    end
 end
